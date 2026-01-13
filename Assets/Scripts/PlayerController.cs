a4c217e1ac3a9d119dd9ec8fa45b55ec13b55028
N1ghtfoxx <2024.gme.naomizel@gmail.com>
2026-01-13 09:40:21 +0100

    UPD script PlayerController - player rotates towards mouse position UPD Player prefab - has now Player tag DEL atomatically generated C# InputSystem_Actions-script because of errors

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using FishNet.Object;
 
 
+
 public class PlayerController : NetworkBehaviour
 {
     [SerializeField] private float playerSpeed;
@@ -9,24 +10,51 @@ public class PlayerController : NetworkBehaviour
 
     public CharacterController cc;
     private InputSystem_Actions inputActions;
+    private Camera mainCamera;
 
     private void Start() 
     { 
         inputActions = new InputSystem_Actions();
         inputActions.Player.Enable();
         TimeManager.OnTick += OnServerTick;
+        mainCamera = Camera.main;
     }
 
     private void OnServerTick() 
     {
-        // read our players movement 
+        // read movement input 
         Vector2 movementInput = inputActions.Player.Move.ReadValue<Vector2>();
 
         // if we are the client and owner, request move
         if (IsClientInitialized && IsOwner){
 
             MoveServerRPC(movementInput);
+
+            // send mouse position to server for rotation
+            Vector3 mouseWorldPos = GetMouseWorldPosition();
+            RotateServerRPC(mouseWorldPos);
         }
+    }
+
+    private Vector3 GetMouseWorldPosition() 
+    {
+        if (mainCamera == null) return transform.position;
+
+        // get mouse position in screen space
+        Vector2 mouseScreenPos = inputActions.Player.Look.ReadValue<Vector2>();
+
+        // create ray from camera through mouse position
+        Ray ray = mainCamera.ScreenPointToRay(mouseScreenPos);
+
+        // plane at y = playerTransform.position.y
+        Plane groundPlane = new Plane(Vector3.up, transform.position);
+
+        // raycast to plane
+        if (groundPlane.Raycast(ray, out float distance)) 
+        {
+            return ray.GetPoint(distance);
+        }
+        return transform.position;
     }
 
     #region Movement
@@ -43,4 +71,30 @@ public class PlayerController : NetworkBehaviour
     }
 
     #endregion
+
+    #region Rotation
+    private void RotateTowards(Vector3 targetPosition)
+    {
+        // direction to look at
+        Vector3 direction = targetPosition - transform.position;
+        direction.y = 0; // keep only horizontal direction
+
+        // only rotate if there is a valid direction
+        if (direction.sqrMagnitude > 0.01f) 
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = targetRotation;
+        }
+    }
+
+    [ServerRpc]
+    private void RotateServerRPC(Vector3 mouseWorldPosition) 
+    {
+        RotateTowards(mouseWorldPosition);
+    }
+
+    #endregion
+
+    /// TODO: Damage, Health
+    /// IDamageable interface übernehmen
 }

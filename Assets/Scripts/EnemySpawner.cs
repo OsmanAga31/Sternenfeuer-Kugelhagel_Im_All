a87c429a960b4323b6a2ae8340d1c892839ae1d0
BTLV444\Osman <2024.gme.osmansen@gmail.com>
2026-01-20 11:38:38 +0100

    ADD Boss Enemy  prefab EDT Enemy script to adapt to being a Boss ADD Boss Enemy to Object  Pool ADD Raids

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,15 +4,23 @@ using System.Collections;
 
 public class EnemySpawner : NetworkBehaviour
 {
+    [Header("Prefabs")]
     [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private GameObject enemyBossPrefab;
 
+    [Header("Spawn Settings")]
     [SerializeField] private int minPlayersToStart = 1;
     [SerializeField] private float spawnInterval = 1f;
     [SerializeField] private float minSpawnRadius = 25f;
     [SerializeField] private float spawnRadius = 50f;
     [SerializeField] private int spawnCountEnemies = 10;
 
-    private string targetPoolName = "Enemies";
+    [Header("Raid & Boss Settings")]
+    [Tooltip("Interval in seconds for raid events")]
+    [SerializeField] private float raidInterval = 60f;
+    [Tooltip("Number of enemies to spawn during a raid event. Not including the Boss.")]
+    [SerializeField] private int raidEnemyCount = 5;
+
     private float spawnX;
     private float spawnZ;
 
@@ -34,28 +42,34 @@ public class EnemySpawner : NetworkBehaviour
         }
 
         StartCoroutine(SpawnDelayed());
+        StartCoroutine(RaidEvent());
     }
 
     [Server]
-    private void SpawnEnemy()
+    private void SpawnEnemy(bool isBoss)
     {
-        // Nutze Polarkoordinaten für einen echten Ring
+        // Spawn at random position within spawn radius in a ring. Code by A.I. (Only the spawn position)
         float angle = Random.Range(0f, 360f) * Mathf.Deg2Rad;
         float distance = Random.Range(minSpawnRadius, spawnRadius);
 
         spawnX = Mathf.Cos(angle) * distance;
         spawnZ = Mathf.Sin(angle) * distance;
 
-        Vector3 spawnPosition = new Vector3(spawnX, 0.5f, spawnZ);
+        Vector3 spawnPosition = new Vector3(spawnX, 0f, spawnZ);
+        NetworkObject poolEnemy;
 
-        NetworkObject poolEnemy = NetworkManager.GetPooledInstantiated(NewObjectPoolManager.Instance.getObject(PoolObjectType.Enemy1), true);
+        if (isBoss)
+        {
+            poolEnemy = NetworkManager.GetPooledInstantiated(NewObjectPoolManager.Instance.getObject(PoolObjectType.EnemyBoss1), true);
+        }
+        else
+        {
+            poolEnemy = NetworkManager.GetPooledInstantiated(NewObjectPoolManager.Instance.getObject(PoolObjectType.Enemy1), true);
+        }
+        
         poolEnemy.transform.position = spawnPosition;
-
-
-
         Spawn(poolEnemy);
-
-        Debug.Log("Enemy spawned on server. " + poolEnemy.name);
+        //Debug.Log("Enemy spawned on server. " + poolEnemy.name);
     }
 
     private IEnumerator SpawnDelayed()
@@ -63,7 +77,23 @@ public class EnemySpawner : NetworkBehaviour
         for (int i = 0; i < spawnCountEnemies; i++)
         {
             yield return new WaitForSeconds(spawnInterval);
-            SpawnEnemy();
+            SpawnEnemy(false);
+        }
+    }
+
+    private IEnumerator RaidEvent()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(raidInterval);
+            
+            for (int i = 0; i < raidEnemyCount; i++)
+            {
+                SpawnEnemy(false);
+            }
+            SpawnEnemy(true); // Spawn Boss
+
+
         }
     }
 
